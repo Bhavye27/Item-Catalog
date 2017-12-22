7c6994dd7d14a8c9656716be3afb00509568f9f5
Rahul Kathuria <rahulkathuria52@gmail.com>
2017-12-23 04:04:38 +0530

    Objective 2 done

--- webserver.py
+++ webserver.py
@@ -26,7 +26,13 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<html><body>"
                 for restaurant in restaurants:
                     output += restaurant.name
-                    output += "</br></br></br>"
+                    output += "</br>"
+                    output += "<a href='#'>Edit</a>"
+                    output += "</br>"
+                    output += "<a href='#'>Delete</a>"
+                    output += "</br>"
+                    output += "</br></br></br>" 
+                    
 
                 output += "</body></html>"
                 self.wfile.write(output)
